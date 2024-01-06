bac463897d6482df5fc5f69529a396175f96c576
Shreyash Satish Killedar <144792072+shreyashkilledar@users.noreply.github.com>
2024-01-07 00:07:10 +0530

    Add files via upload

---
+++ WPT_Exam Practice/JavaScript/fourth.js
@@ -0,0 +1,15 @@
+let obj={
+    fname:"shreyash",
+    lname:"killedar",
+    age:23,
+    area:{
+        city:"karad",
+        dist:"satara",
+        state:"maharashtra",
+        country:"india",
+    }
+}
+
+console.log(obj.fname)
+console.log(obj.lname)
+console.log(obj.age)
