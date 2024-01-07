c9d0200bffe744c3117ac3d4af95ea3f719fb5b0
Shreyash Satish Killedar <144792072+shreyashkilledar@users.noreply.github.com>
2024-01-08 00:11:33 +0530

    Add files via upload

---
+++ WPT_1/js/sec.js
@@ -0,0 +1,6 @@
+function add(x,y)
+{
+    return x+y;
+}
+let result = add(25,32);
+console.log(result);
---
+++ WPT_1/js/third.js
@@ -0,0 +1,7 @@
+function sum(x,y)
+{
+    return x+y;
+}
+console.log(sum(12,22))
+console.log(sum(20,33))
+console.log(sum(12,23,45))
