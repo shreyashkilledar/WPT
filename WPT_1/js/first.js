b8231601f49e053cb71c3969587f844477a1ed03
Shreyash Satish Killedar <144792072+shreyashkilledar@users.noreply.github.com>
2024-01-08 00:12:04 +0530

    Add files via upload

---
+++ WPT_1/js/first.js
@@ -0,0 +1,4 @@
+console.log("Hello World")
+let x
+x=10
+console.log(x)
---
+++ WPT_1/js/fourth.js
@@ -0,0 +1,15 @@
+let obj={
+    fname:"shreyash",
+    lname:"killedar",
+    age:23,
+    area:{
+        city:"karad",
+        dist:"satara",
+        state:"maharashtra",
+        country:"india",
+    }
+}
+
+console.log("FirstName: ",obj.fname)
+console.log("LastName: ",obj.lname)
+console.log("Age: ",obj.age)
